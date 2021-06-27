184bba47977391650bdb064b22411d4e87c1dbc6
git_yusuke <g10tthf9d9@hi3.enjoy.ne.jp>
2021-06-27 11:40:45 +0900

    山田担当分反映

---
+++ EquipmentBookingSystem/src/state/StandbyUserProcedureInput.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+
+
+import state as state
+import dev.display.Console as Console
+import dev.input as input
+
+class StandbyUserProcedureInput(state.IState):
+    def entry(self):
+        Console.clear()
+        Console.puts("手続き内容を選択してください","1:借用","2:返却","3:更新")
+        Console.puts(">", end="")
+        self.__input = input.UserInputReader()
+        self.__get_next_state = state.ErrorHasOccurred()
+
+    def do(self):
+        self.__input.capture()
+
+    def exit(self):
+        procedure_type = self.__input.get_string()
+        if( "1" == procedure_type ):
+            Console.puts("借用ですね")
+ #           self.__get_next_state = state.StandbyUserIdInput()
+        elif( "2" == procedure_type ):
+            Console.puts("返却ですね")
+        elif( "3" == procedure_type ):
+            Console.puts("更新ですね")
+        else:
+            Console.puts("無効な入力ですね")
+
+ #       state.CommonResource.enquiry = procedure_type
+
+    def get_next_state(self):
+        return self.__get_next_state
+
+    def should_exit(self):
+        return self.__input.submitted()
