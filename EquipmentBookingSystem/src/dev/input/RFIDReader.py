ace20293fbe25f70cdd9fea59ee70363351b7dff
Osamu-Sano <79613063+Osamu-Sano@users.noreply.github.com>
2021-07-06 17:17:30 +0900

    Update RFIDReader.py
    RFID読み込み処理追加
    　カード固有のRFIDを戻り値として返す

--- EquipmentBookingSystem/src/dev/input/RFIDReader.py
+++ EquipmentBookingSystem/src/dev/input/RFIDReader.py
@@ -1,26 +1,50 @@
-#!/usr/bin/env python
-
-
 if __name__ == "__main__":
     import time
     import sys
     sys.path.append('../')
     sys.path.append('../../')
 
-import dev.display as display
-import dev.input as input
+##import dev.display as display
+##import dev.input as input
 
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+
+# Create an object of the class MFRC522
+# MIFAREReader = MFRC522.MFRC522()
 
 # Not implemented yet.
-class RFIDReader(input.IUserInputReader):
+class RFIDReader():
+##class RFIDReader(input.IUserInputReader):
+    
     def __init__(self):
+        continue_reading = True
         pass
-
+    
+    #
     def capture(self):
+        
+        #(status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+        #(status,uid) = MIFAREReader.MFRC522_Anticoll()
+        
         pass
-
+    
     def get_string(self):
-        return ""
+        
+        MIFAREReader = MFRC522.MFRC522()
+        continue_reading = True
+        while continue_reading:
+        
+            (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+            (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+            if status == MIFAREReader.MI_OK:
+
+                ret_uid = str(uid[0]) + str(uid[1]) + str(uid[2]) + str(uid[3])
+                continue_reading = False
+        GPIO.cleanup()
+        return ret_uid 
 
     def submitted(self):
         return False
@@ -34,3 +58,5 @@ if __name__ == "__main__":
     help(debug_this_module)
     time.sleep(1)
     debug_this_module()
+    ret_dat = RFIDReader.get_string("")
+    print(ret_dat)
