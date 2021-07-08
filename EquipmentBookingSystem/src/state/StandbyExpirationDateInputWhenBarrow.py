9e0a3d06a504d56ad969d430cd090c630b01be85
Jun-nishihara <86098894+Jun-nishihara@users.noreply.github.com>
2021-07-08 17:10:40 +0900

    Create StandbyExpirationDateInputWhenBarrow.py
    西原担当分反映

---
+++ EquipmentBookingSystem/src/state/StandbyExpirationDateInputWhenBarrow.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+
+import state as state
+import dev.display.Console as Console
+import dev.input as input
+
+class StandbyUserProcedureInput(state.IState):
+    def entry(self):
+        Console.clear()
+        Console.puts("返却予定日を入力してください(yyyy/mm/dd)")
+        Console.puts(">", end="")
+        self.__input = input.UserInputReader()
+        self.__get_next_state = state.ErrorHasOccurred()
+
+    def do(self):
+        self.__input.capture()
+
+    def exit(self):
+        return_date = self.__input.get_string()
+        if( "1" == procedure_type ):
+            Console.puts("返却予定日")
+            Console.puts(return_date)
+　　　      Console.puts("を受理しました")
+        state.CommonResource.expirationDate = return_date
+
+    def get_next_state(self):
+        return state.SuccessBarrowEquipment()
+
+    def should_exit(self):
+        return self.__input.submitted()
