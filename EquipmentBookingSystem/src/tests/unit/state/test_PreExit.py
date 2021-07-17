9561cd8c71ee8a47f7a7f7b4abe833e6c605992b
shiftdownet <k11096kk@outlook.com>
2021-07-17 15:39:36 +0900

    Unit test sample codes were added.

---
+++ EquipmentBookingSystem/src/tests/unit/state/test_PreExit.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import state
+
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+
+class TestPreExit(TestCase):
+
+    def setUp(self):
+        self.state = state.PreExit()
+        self.state.entry()
+
+    def tearDown(self):
+        pass
+
+    def test_exit_by_key(self):
+        # Test the conditions of exit from this state.
+        with patch("dev.input.PressedKey.exists") as mock_pressedKey_exists:
+            # If pressed key does not exists, state should not exit.
+            mock_pressedKey_exists.return_value = False
+            self.state.do()
+            self.assertEqual(self.state.should_exit(), False)
+
+            # If pressed key exists, state should exit.
+            mock_pressedKey_exists.return_value = True
+            self.state.do()
+            self.assertEqual(self.state.should_exit(), True)
+
+        # Test the state of the next transition destination.
+        with patch("dev.input.PressedKey.is_escape") as mock_pressedKey_is_escape:
+            # if pressed key is not espace, next state is Init.
+            mock_pressedKey_is_escape.return_value = False
+            self.state.do()
+            self.assertIsInstance(self.state.get_next_state(), state.Init)
+
+            # if pressed key is espace, next state is Exit.
+            mock_pressedKey_is_escape.return_value = True
+            self.state.do()
+            self.assertIsInstance(self.state.get_next_state(), state.Exit)
