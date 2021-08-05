0e25429d2179e05b23a86b168a69becb57a4c34c
yasue-akira <akira.yasue@aisin-software.com>
2021-08-05 19:09:05 +0900

    データベース関数の中身を実装

--- EquipmentBookingSystem/src/db/Registration/DBregistration.py
+++ EquipmentBookingSystem/src/db/Registration/DBregistration.py
@@ -1,24 +1,100 @@
 #!/usr/bin/env python
 
+import os
+import pandas as pd
+from datetime import datetime
+
+filepath = os.path.dirname(__file__)
+# pathlib.Pathで相対パスを絶対パスに変換したかったがうまく動かなかったので代替手段で実装
+# ひとまず「src」フォルダ名で切る。
+spfldpath = filepath.split("src")
+filepath_user = os.path.join(spfldpath[0],"database","db_userlist.csv")
+filepath_item = os.path.join(spfldpath[0],"database","db_itemlist.csv")
+
 def DBregistration_EmpIDtoEmpNo(EmpID,EmpNo):
     # 社員証から読み取ったID(EmpID)が手打ちした社員番号に紐付くようユーザーを登録したDBに登録する
     # 登録できた場合「True」と返す
     # 登録できなかった場合「False」と返す
 
-    # 仮の戻り値
-    ret = True
+    # encodingはshift-jisがエラー出るのでcp932を使用
+    df_csv = pd.read_csv(filepath_user,encoding = "cp932")
+    
+    # 空白が欠損値NaNで取り込まれたままだと後工程でエラーとなるので「-」で埋める
+    df_tmpcsv = df_csv.fillna("-")
+
+    # 処理内容は同じはずだがうまく動かなかったので記載方法変更。
+    #df_tgt = df_tmpcsv.query('ユーザID == {}'.format(EmpNo))
+    df_tgt = df_tmpcsv.query('ユーザID == @EmpNo')
+
+    # DB登録と戻り値設定
+    if len(df_tgt) == 1:
+        # 書き戻すデータは「-」埋めしてないデータにする。
+        df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('備考')] = EmpID
+        df_csv.to_csv(filepath_user, index=False, header=True, encoding="cp932", mode='w')
+
+        # バックアップを出力
+        # タイムスタンプは「年月日_時分秒」で取得
+        add_time = datetime.now().strftime('%Y%m%d_%H%M%S_')
+        bkupfilepath = filepath_user.replace(os.path.basename(filepath_user),"BKUP_" + add_time + os.path.basename(filepath_user))
+        df_csv.to_csv(bkupfilepath, index=False, header=True, encoding="cp932", mode='w')
+
+        ret = True
+    elif len(df_tgt) > 1:
+        # 不正登録（多重登録）はないはずなので仮実装※登録失敗で返す
+        ret = False
+    else:
+        # DBに登録されてない場合は登録しない。
+        ret = False
 
     return ret
 
 def DBregistration_Borrow(EquID,EmpNo,RetDay):
     # 備品を登録したDBの備品ステータスを借用中に登録する
-    #   対象備品(RquID)と社員番号（EquNo）と返却予定日(RetDay)は必須
+    #   対象備品(EquID)と社員番号（EmpNo）と返却予定日(RetDay)は必須
     #   借用開始日は自動取得
     # 登録できた場合「True」と返す
     # 登録できなかった場合「False」と返す
     
-    # 仮の戻り値
-    ret = True
+    # encodingはshift-jisがエラー出るのでcp932を使用
+    df_csv = pd.read_csv(filepath_item,encoding = "cp932")
+    
+    # 空白が欠損値NaNで取り込まれたままだと後工程でエラーとなるので「-」で埋める
+    df_tmpcsv = df_csv.fillna("-")
+
+    # 処理内容は同じはずだがうまく動かなかったので記載方法変更。
+    df_tgt = df_tmpcsv.query('タグID == {}'.format(EquID))
+    #df_tgt = df_tmpcsv.query('タグID == @EquID')
+
+    # 借用開始日は本手続きをした日で固定
+    brw_day = datetime.now().strftime('%Y/%m/%d')
+
+    # DB登録と戻り値設定
+    if len(df_tgt) == 1:
+        #借用可能なら貸し出し処理する
+        if df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('内容')] == "借用可能":
+            # 書き戻すデータは「-」埋めしてないデータにする。
+            df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('ユーザID')] = EmpNo
+            df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('開始日付')] = brw_day
+            df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('終了日付')] = RetDay
+            df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('内容')] = "借用中"
+            df_csv.to_csv(filepath_item, index=False, header=True, encoding="cp932", mode='w')
+
+            # バックアップを出力
+            # タイムスタンプは「年月日_時分秒」で取得
+            add_time = datetime.now().strftime('%Y%m%d_%H%M%S_')
+            bkupfilepath = filepath_item.replace(os.path.basename(filepath_item),"BKUP_" + add_time + os.path.basename(filepath_item))
+            df_csv.to_csv(bkupfilepath, index=False, header=True, encoding="cp932", mode='w')
+
+            ret = True
+        else:
+            ret = False
+
+    elif len(df_tgt) > 1:
+        # 不正登録（多重登録）はないはずなので仮実装※登録失敗で返す
+        ret = False
+    else:
+        # DBに登録されてない場合は登録しない。
+        ret = False
 
     return ret
 
@@ -28,8 +104,43 @@ def DBregistration_Return(EquID):
     # 登録できた場合「True」と返す
     # 登録できなかった場合「False」と返す
     
-    # 仮の戻り値
-    ret = True
+    # encodingはshift-jisがエラー出るのでcp932を使用
+    df_csv = pd.read_csv(filepath_item,encoding = "cp932")
+    
+    # 空白が欠損値NaNで取り込まれたままだと後工程でエラーとなるので「-」で埋める
+    df_tmpcsv = df_csv.fillna("-")
+
+    # 処理内容は同じはずだがうまく動かなかったので記載方法変更。
+    df_tgt = df_tmpcsv.query('タグID == {}'.format(EquID))
+    #df_tgt = df_tmpcsv.query('タグID == @EquID')
+
+    # DB登録と戻り値設定
+    if len(df_tgt) == 1:
+        #借用中なら返却処理する
+        if df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('内容')] == "借用中":
+            # 書き戻すデータは「-」埋めしてないデータにする。
+            df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('ユーザID')] = "-"
+            df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('開始日付')] = "-"
+            df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('終了日付')] = "-"
+            df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('内容')] = "借用可能"
+            df_csv.to_csv(filepath_item, index=False, header=True, encoding="cp932", mode='w')
+
+            # バックアップを出力
+            # タイムスタンプは「年月日_時分秒」で取得
+            add_time = datetime.now().strftime('%Y%m%d_%H%M%S_')
+            bkupfilepath = filepath_item.replace(os.path.basename(filepath_item),"BKUP_" + add_time + os.path.basename(filepath_item))
+            df_csv.to_csv(bkupfilepath, index=False, header=True, encoding="cp932", mode='w')
+
+            ret = True
+        else:
+            ret = False
+
+    elif len(df_tgt) > 1:
+        # 不正登録（多重登録）はないはずなので仮実装※登録失敗で返す
+        ret = False
+    else:
+        # DBに登録されてない場合は登録しない。
+        ret = False
 
     return ret
 
@@ -39,20 +150,64 @@ def DBregistration_Update(EquID,RetDay):
     # 登録できた場合「True」と返す
     # 登録できなかった場合「False」と返す
     
-    # 仮の戻り値
-    ret = True
+    # encodingはshift-jisがエラー出るのでcp932を使用
+    df_csv = pd.read_csv(filepath_item,encoding = "cp932")
+    
+    # 空白が欠損値NaNで取り込まれたままだと後工程でエラーとなるので「-」で埋める
+    df_tmpcsv = df_csv.fillna("-")
+
+    # 処理内容は同じはずだがうまく動かなかったので記載方法変更。
+    df_tgt = df_tmpcsv.query('タグID == {}'.format(EquID))
+    #df_tgt = df_tmpcsv.query('タグID == @EquID')
+
+    # DB登録と戻り値設定
+    if len(df_tgt) == 1:
+        #借用中なら返却日更新処理する
+        if df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('内容')] == "借用中":
+            # 書き戻すデータは「-」埋めしてないデータにする。
+            df_csv.iat[df_tgt.index.values[0],df_tgt.columns.get_loc('終了日付')] = RetDay
+            df_csv.to_csv(filepath_item, index=False, header=True, encoding="cp932", mode='w')
+
+            # バックアップを出力
+            # タイムスタンプは「年月日_時分秒」で取得
+            add_time = datetime.now().strftime('%Y%m%d_%H%M%S_')
+            bkupfilepath = filepath_item.replace(os.path.basename(filepath_item),"BKUP_" + add_time + os.path.basename(filepath_item))
+            df_csv.to_csv(bkupfilepath, index=False, header=True, encoding="cp932", mode='w')
+
+            ret = True
+        else:
+            ret = False
+
+    elif len(df_tgt) > 1:
+        # 不正登録（多重登録）はないはずなので仮実装※登録失敗で返す
+        ret = False
+    else:
+        # DBに登録されてない場合は登録しない。
+        ret = False
 
     return ret
 
 if __name__ == "__main__":
-    ret = DBregistration_EmpIDtoEmpNo("114","514")
+    #ret = DBregistration_EmpIDtoEmpNo("181818","79348")
+    #print(ret)
+
+    ret = DBregistration_Borrow("101","79348","2021/08/26")
+    print(ret)
+    ret = DBregistration_Borrow("102","79348","2021/08/26")
+    print(ret)
+    ret = DBregistration_Borrow("201","79348","2021/08/26")
     print(ret)
 
-    ret = DBregistration_Borrow("11","45","14")
+    ret = DBregistration_Return("102")
     print(ret)
-
-    ret = DBregistration_Return("114514")
+    ret = DBregistration_Return("201")
+    print(ret)
+    ret = DBregistration_Return("202")
     print(ret)
     
-    ret = DBregistration_Update("114","514")
+    ret = DBregistration_Update("101","2021/09/01")
+    print(ret)
+    ret = DBregistration_Update("102","2021/09/01")
+    print(ret)
+    ret = DBregistration_Update("201","2021/09/01")
     print(ret)
--- EquipmentBookingSystem/src/db/matching/DBmatching.py
+++ EquipmentBookingSystem/src/db/matching/DBmatching.py
@@ -1,32 +1,120 @@
 #!/usr/bin/env python
 
+import os
+import pandas as pd
+
+filepath = os.path.dirname(__file__)
+# pathlib.Pathで相対パスを絶対パスに変換したかったがうまく動かなかったので代替手段で実装
+# ひとまず「src」フォルダ名で切る。
+spfldpath = filepath.split("src")
+filepath_user = os.path.join(spfldpath[0],"database","db_userlist.csv")
+filepath_item = os.path.join(spfldpath[0],"database","db_itemlist.csv")
+
 def DBmatching_EmpIDtoEmpNo(EmpID):
     # 社員証から読み取ったID(EmpID)をユーザーを登録したDBと照合する
     # 登録済みユーザーなら社員番号を返す
     # 未登録ユーザーなら「False」と返す
     
-    # 仮の戻り値
-    EmpNo = "0079522"
+    # encodingはshift-jisがエラー出るのでcp932を使用
+    df_csv = pd.read_csv(filepath_user,encoding = "cp932")
+    
+    # 空白が欠損値NaNで取り込まれたままだと後工程でエラーとなるので「-」で埋める
+    df_csv = df_csv.fillna("-")
+
+    df_Emp = df_csv.query('備考 == {}'.format(EmpID))
+
+    # 戻り値設定
+    if len(df_Emp) == 1:
+        EmpNo = df_Emp.iat[0,df_Emp.columns.get_loc('ユーザID')]
+    elif len(df_Emp) > 1:
+        # 不正登録（多重登録）はないはずなので仮実装※未登録で返す
+        EmpNo = False
+    else:
+        EmpNo = False
 
     return EmpNo
 
 def DBmatching_EquIDtoEquStatus(EquID):
     # 備品タグから読み取ったIDを備品を登録したDBと照合する
     # 登録済み備品なら借用状況を返す
+    #   0：未登録
     #   1：借用可能
     #   2：借用中
     #   3：故障中
-    # 未登録備品なら「False」と返す
+    #   4：DBメンテミス
+        
+    # encodingはshift-jisがエラー出るのでcp932を使用
+    df_csv = pd.read_csv(filepath_item,encoding = "cp932")
     
-    # 仮の戻り値
-    EquStatus = 1
+    # 空白が欠損値NaNで取り込まれたままだと後工程でエラーとなるので「-」で埋める
+    df_csv = df_csv.fillna("-")
+
+    df_Equ = df_csv.query('タグID == {}'.format(EquID))
+
+    # 戻り値設定
+    if len(df_Equ) == 1:
+        if df_Equ.iat[0,df_Equ.columns.get_loc('内容')] == "借用可能": 
+            EquStatus = 1
+        elif df_Equ.iat[0,df_Equ.columns.get_loc('内容')] == "借用中":
+            EquStatus = 2
+        elif df_Equ.iat[0,df_Equ.columns.get_loc('内容')] == "故障中":
+            EquStatus = 3
+        else:
+            # DBの「内容」欄のメンテミスを想定
+            EquStatus = 4
+
+    elif len(df_Equ) > 1:
+        # 不正登録（多重登録）はないはずなので仮実装※未登録で返す
+        EquStatus = 0
+    else:
+        EquStatus = 0
 
     return EquStatus
+
+def DBmatching_EquIDtoEquName(EquID):
+    # 備品タグから読み取ったIDを備品を登録したDBと照合する
+    # 登録済み備品なら備品名を返す
+    # 未登録備品なら「False」と返す
+        
+    # encodingはshift-jisがエラー出るのでcp932を使用
+    df_csv = pd.read_csv(filepath_item,encoding = "cp932")
+    
+    # 空白が欠損値NaNで取り込まれたままだと後工程でエラーとなるので「-」で埋める
+    df_csv = df_csv.fillna("-")
+
+    df_Equ = df_csv.query('タグID == {}'.format(EquID))
+
+    # 戻り値設定
+    if len(df_Equ) == 1:
+        EquName = df_Equ.iat[0,df_Equ.columns.get_loc('施設ID')]
+    elif len(df_Equ) > 1:
+        # 不正登録（多重登録）はないはずなので仮実装※未登録で返す
+        EquName = False
+    else:
+        EquName = False
+
+    return EquName
 
 if __name__ == "__main__":
     ret = DBmatching_EmpIDtoEmpNo("114514")
     print(ret)
-
-    ret = DBmatching_EquIDtoEquStatus("114514")
+    ret = DBmatching_EmpIDtoEmpNo("000000")
     print(ret)
 
+    ret = DBmatching_EquIDtoEquStatus("101")
+    print(ret)
+    ret = DBmatching_EquIDtoEquStatus("102")
+    print(ret)
+    ret = DBmatching_EquIDtoEquStatus("201")
+    print(ret)
+    ret = DBmatching_EquIDtoEquStatus("302")
+    print(ret)
+
+    ret = DBmatching_EquIDtoEquName("101")
+    print(ret)
+    ret = DBmatching_EquIDtoEquName("102")
+    print(ret)
+    ret = DBmatching_EquIDtoEquName("201")
+    print(ret)
+    ret = DBmatching_EquIDtoEquName("302")
+    print(ret)
