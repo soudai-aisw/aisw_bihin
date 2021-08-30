55320e440240caaab63e6bacde7890ab77875107
shiftdownet <k11096kk@outlook.com>
2021-08-30 22:44:03 +0900

    Display nomination at user authentication. Prevent query injection.

--- EquipmentBookingSystem/src/state/StandbyUserIdInput.py
+++ EquipmentBookingSystem/src/state/StandbyUserIdInput.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-
-import time
 import state as state
 import dev.display.Console as Console
 import dev.input as input
-import db.matching.DBmatching as db
+from db.AccountLedger import AccountLedger
+from db.AccountRecord import AccountRecord
 
 
 class StandbyUserIdInput(state.IState):
@@ -13,30 +12,41 @@ class StandbyUserIdInput(state.IState):
         Console.clear()
         Console.puts("社員証をかざしてください")
         Console.puts(">", end="")
-        self.__input = input.UserInputReader()
+        self.__input = input.SunitizedString(
+            input.UserInputReader()
+        )
+
         self.__get_next_state = state.ErrorHasOccurred()
 
     def do(self):
         self.__input.capture()
 
     def exit(self):
-        employee_id = self.__input.get_string()
-        if (employee_id != ""):
-            match_id = db.DBmatching_EmpIDtoEmpNo(employee_id)
+        rfid = self.__input.get_string()
+        print(rfid)
+        if (rfid != ""):
+            records = AccountLedger().find_by(AccountRecord.RFID, rfid)
 
-            # False以外=登録済みユーザー
-            if (match_id != False):
-                # ここに社員名をDBから取得して表示したい（レベルアップ）
-              #  name = db.名前取得(match_id)
-                Console.puts("ようこそ", match_id, "さん", "\n")
-                state.CommonResource.employeeId = match_id
+            if len(records) == 0:
+                Console.puts("存在しない社員番号です")
+                Console.puts("再度試しても失敗する場合、システム管理者に問い合わせてください。")
+                self.__get_next_state = state.ErrorHasOccurred()
+            elif len(records) == 1:
+                record = records.loc[0]
+                Console.puts(
+                    "ようこそ、社員番号",
+                    record[AccountRecord.EMPLOYEE_ID],
+                    "の",
+                    record[AccountRecord.LAST_NAME],
+                    record[AccountRecord.FIRST_NAME],
+                    "さん", "\n")
+                state.CommonResource.employeeId = \
+                    record[AccountRecord.EMPLOYEE_ID]
                 self.__get_next_state = state.GotoNextAfterWaiting()
                 self.__get_next_state.set_next_state(
                     state.StandbyUserProcedureInput())
             else:
-                Console.puts("存在しない社員番号です")
-                Console.puts("再度試しても失敗する場合、システム管理者に問い合わせてください。")
-                self.__get_next_state = state.ErrorHasOccurred()
+                raise SystemError("AccountLedger returned multiple records.")
 
         else:
             self.__get_next_state = state.StandbyUserIdInput()
